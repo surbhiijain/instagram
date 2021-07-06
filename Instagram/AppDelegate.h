ec55fcae280430f3d33e3c0398494b05fe15b5db
Surbhi Jain <30298846+surbhiijain@users.noreply.github.com>
2021-07-06 10:27:21 -1000

    Initial Commit

---
+++ Instagram/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Instagram
+//
+//  Created by Surbhi Jain on 7/6/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Instagram/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Instagram
+//
+//  Created by Surbhi Jain on 7/6/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
